3dc9bdced521bf8ca8c2a4debaf86b6a484d4c28
Jaspreet Singh <jaspreet98singh@gmail.com>
2022-04-21 16:41:49 +0530

    text color black to gray-800

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ export default function Home() {
 
     return (
         <main
-            className="container flex flex-col items-center justify-between w-full px-4 mx-auto mt-10 xl:flex-row xl:justify-evenly">
+            className="container flex flex-col items-center justify-between w-full px-4 mx-auto mt-10 text-gray-800 xl:flex-row xl:justify-evenly">
             <img className="mb-20 rounded-full shadow xl:mb-0 w-80 xl:w-72 xl:h-72" src={DP} alt="Shubham" />
             <div className="max-w-3xl xl:pl-8">
                 <div className="font-semibold text-violet-600">HELLO THERE,</div>
