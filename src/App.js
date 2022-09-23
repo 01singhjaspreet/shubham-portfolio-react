35430739954b89f206ee831c6453fd83ee602b1b
Jaspreet Singh <jaspreet98singh@gmail.com>
2022-09-23 22:05:57 +0530

    HOTFIX

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Nav from './components/nav';
 import Home from './components/home';
 import Footer from './components/footer';
 import Work from './components/work';
-import WeddingProject from './components/weddingProject';
 import MentorProject from './components/mentorProject';
 import MetalProject from './components/metalnClayLogoProject';
 import KathasProject from './components/kathasEntertainmentLogoProject';
@@ -21,7 +20,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
-        <Route path="/wedding-project" element={<WeddingProject />} />
         <Route path="/mentor-project" element={<MentorProject />} />
         <Route path="/metal-n-clay-logo-project" element={<MetalProject />} />
         <Route path="/kathas-entertainment-logo-project" element={<KathasProject />} />
