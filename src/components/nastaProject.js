7b70037e7f9a7ddc082ac3b7edc4f9f42a697523
Jaspreet Singh <jaspreet98singh@gmail.com>
2022-09-23 22:34:57 +0530

    button update

--- src/components/nastaProject.js
+++ src/components/nastaProject.js
@@ -126,10 +126,10 @@ export default function MentorProject() {
           <p className="ml-12 ">
             I have developed the HTML and CSS code for the website and Jaspreet Singh implimented the required JS of the website.
           </p>
-          <a className='inline-flex items-center gap-2 px-4 py-2.5 mt-12 ml-12 text-xl font-medium border-2 border-violet-600 text-violet-600 transition-all rounded-lg hover:bg-violet-600 hover:text-white' target="_blank" href='https://nasta-shubham.netlify.app/' rel='noopener noreferrer'>View Project
-          <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="24px" height="24px">
-            <path d="M 40.960938 4.9804688 A 2.0002 2.0002 0 0 0 40.740234 5 L 28 5 A 2.0002 2.0002 0 1 0 28 9 L 36.171875 9 L 22.585938 22.585938 A 2.0002 2.0002 0 1 0 25.414062 25.414062 L 39 11.828125 L 39 20 A 2.0002 2.0002 0 1 0 43 20 L 43 7.2460938 A 2.0002 2.0002 0 0 0 40.960938 4.9804688 z M 12.5 8 C 8.3826878 8 5 11.382688 5 15.5 L 5 35.5 C 5 39.617312 8.3826878 43 12.5 43 L 32.5 43 C 36.617312 43 40 39.617312 40 35.5 L 40 26 A 2.0002 2.0002 0 1 0 36 26 L 36 35.5 C 36 37.446688 34.446688 39 32.5 39 L 12.5 39 C 10.553312 39 9 37.446688 9 35.5 L 9 15.5 C 9 13.553312 10.553312 12 12.5 12 L 22 12 A 2.0002 2.0002 0 1 0 22 8 L 12.5 8 z" /></svg>
-        </a>
+          <a className='inline-flex items-center gap-2 px-3 py-2 mt-12 ml-12 text-lg font-medium transition-all border-2 rounded-lg border-violet-600 text-violet-600 hover:bg-violet-600 hover:text-white' target="_blank" href='https://nasta-shubham.netlify.app/' rel='noopener noreferrer'>View Project
+            <svg fill="currentColor" strokeWidth='1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="20px" height="20px">
+              <path stroke="currentColor" d="M 40.960938 4.9804688 A 2.0002 2.0002 0 0 0 40.740234 5 L 28 5 A 2.0002 2.0002 0 1 0 28 9 L 36.171875 9 L 22.585938 22.585938 A 2.0002 2.0002 0 1 0 25.414062 25.414062 L 39 11.828125 L 39 20 A 2.0002 2.0002 0 1 0 43 20 L 43 7.2460938 A 2.0002 2.0002 0 0 0 40.960938 4.9804688 z M 12.5 8 C 8.3826878 8 5 11.382688 5 15.5 L 5 35.5 C 5 39.617312 8.3826878 43 12.5 43 L 32.5 43 C 36.617312 43 40 39.617312 40 35.5 L 40 26 A 2.0002 2.0002 0 1 0 36 26 L 36 35.5 C 36 37.446688 34.446688 39 32.5 39 L 12.5 39 C 10.553312 39 9 37.446688 9 35.5 L 9 15.5 C 9 13.553312 10.553312 12 12.5 12 L 22 12 A 2.0002 2.0002 0 1 0 22 8 L 12.5 8 z" /></svg>
+          </a>
         </div>
         <div className="order-1 col-span-12 mt-6 lg:col-span-6 lg:col-start-7 lg:order-2 lg:mt-0">
           <img src={require("../assets/nasta/1.png")} className="w-full rounded" alt="" />
