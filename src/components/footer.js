fa7d8d9115565b8dadf44301489fcd5065642229
Jaspreet Singh <jaspreet98singh@gmail.com>
2022-10-01 22:50:26 +0530

    footer and color code updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,35 @@
 import React from 'react';
-import Linkedin from '../assets/social/linkedin.svg';
-import Instagram from '../assets/social/instagram.svg';
 
 export default function Footer() {
-    return (
-        <footer className="flex flex-col items-center w-full pt-10 mt-20 bg-gray-800">
-            <div className="text-4xl font-bold text-white xl:text-5xl">
-                Let's get in touch
-            </div>
-            <a className="inline-block mt-2 text-white underline"
-                href="mailto:shubhammourya148@gmail.com">shubhammourya148@gmail.com</a>
-            <div className="flex items-center justify-center w-full mt-4">
-                <a href="https://www.linkedin.com/in/shubham-mourya-89193a217/" className="mx-2">
-                    <img src={Linkedin} alt="" />
-                </a>
-                <a href="https://www.instagram.com/amateurdesi9ner/" className="mx-2">
-                    <img src={Instagram} alt="" />
-                </a>
-            </div>
-            <div className="flex flex-col items-center w-full px-4 py-5 mt-10 text-sm bg-white">
-                <p>
-                    Designed with &#x2764; by me. Developed by
-                    <a href="https://jaspreetportfolio.netlify.app/" className="underline ml-0.5">Jaspreet</a>
-                </p>
-                <div>Copyright © Shubham Mourya.</div>
-                <div className="mt-6 text-xs">
-                    Icons courtesy of FlatIcon. Images courtesy of Unsplash and Freepik.
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="flex flex-col items-center w-full pt-6 mt-20 text-white bg-violet-1000">
+      <div className="text-3xl font-bold xl:text-4xl">
+        Let's get in touch
+      </div>
+      <div className='flex items-center gap-2 mt-2'>
+        <a className="inline-block underline"
+          href="mailto:shubhammourya148@gmail.com">Email</a>
+        <span> |</span>
+
+        <a href="https://www.linkedin.com/in/shubham-mourya-89193a217/" target="_blank" rel="noreferrer" className="inline-block underline">
+          Linkedin
+        </a>
+        <span> |</span>
+        <a href="https://www.instagram.com/amateurdesi9ner/" target="_blank" rel="noreferrer" className="inline-block underline">
+          Instagram
+        </a>
+      </div>
+      <div className="flex flex-col items-center w-full px-4 py-5 pt-0 mt-6 text-sm">
+        <div>
+          <span>
+            Designed with &#x2764; by me. Developed by
+            <a href="https://jaspreetportfolio.netlify.app/" className="underline ml-0.5">Jaspreet</a>
+          </span>
+        </div>
+        <div className="mt-2 text-xs">
+          Icons courtesy of FlatIcon. Images courtesy of Unsplash and Freepik.
+        </div>
+      </div>
+    </footer>
+  )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ export default function Home() {
             className="container flex flex-col items-center justify-between w-full px-4 mx-auto mt-10 text-gray-800 xl:flex-row xl:justify-evenly">
             <img className="mb-20 rounded-full shadow xl:mb-0 w-80 xl:w-72 xl:h-72" src={DP} alt="Shubham" />
             <div className="max-w-3xl xl:pl-8">
-                <div className="font-semibold text-violet-600">HELLO THERE,</div>
+                <div className="font-semibold text-violet-700">HELLO THERE,</div>
                 <div className="font-bold text-7xl">I'm Shubham</div>
                 <p className="mt-4 leading-5">
                     I am a UI/UX designer based in Hyderabad, India. I am a B.Sc Computer
